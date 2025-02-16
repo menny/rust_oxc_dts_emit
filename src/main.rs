a5bdd20e5445925b480035fe96309c41f60acd76
Menny Even Danan <menny@evendanan.net>
2025-02-16 15:17:36 -0500

    Initial CI workflow

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,21 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Err("Incorrect number of arguments".into());
     }
 
-    let success = args.chunks_exact(2) // Iterate over pairs directly
+    let success = args
+        .chunks_exact(2) // Iterate over pairs directly
         .map(|chunk| (&chunk[0], &chunk[1])) // Convert to &str
         .map(|(input_path, output_path)| {
             std::fs::read_to_string(input_path)
-              .and_then(|contents| _emit_dts(&contents))
-              .and_then(|dts_contents| std::fs::write(&output_path, dts_contents))
-              .map_err(|e| {
-                  eprintln!("Error processing {}: {}", input_path, e);
-                  e
-              })
+                .and_then(|contents| _emit_dts(&contents))
+                .and_then(|dts_contents| std::fs::write(&output_path, dts_contents))
+                .map_err(|e| {
+                    eprintln!("Error processing {}: {}", input_path, e);
+                    e
+                })
         })
         .all(|r| r.is_ok());
-      
 
-      if success {
+    if success {
         Ok(())
     } else {
         Err("dts emit failure".into())
